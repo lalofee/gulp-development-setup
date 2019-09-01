ae20a7b095b9f3c949f7faa8301151f0a08b45e9
lalofee <armstorfer@silverserver.at>
2019-09-01 12:21:06 +0200

    final setup
    works with partials/modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+// const rename = require('gulp-rename');
 
 // compile scss into css
 function style() {
@@ -14,6 +15,16 @@ function style() {
     .pipe(browserSync.stream());
 }
 
+// function rename() {
+//     return gulp.src('./css/main.css')
+//     .pipe(rename(function (path)
+//     {
+//         path.basename += 'styles.min';
+//         path.extname = '.css';
+//     }))
+//     .pipe(gulp.dest('./css'));
+// }
+
 function watch() {
     browserSync.init({
         server: {
@@ -21,7 +32,7 @@ function watch() {
         }
     });
     gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./css/*.css').on('change', browserSync.reload);
+    // gulp.watch('./css/main.css').on('change', browserSync.reload);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
